0e77d06536f98204422d97bdedfe50a1b5a423c0
Danylo Karpenko <karpenkodanylo@gmail.com>
2020-09-11 18:19:24 +0300

    DS-3: Change arg name

--- email.ts
+++ email.ts
@@ -1,5 +1,5 @@
 export const normalizeEmail: (txt: string) => string = (text) => text.trim().toLowerCase();
 
-export const areEmailsEqual = (email1: string, email2: string) => {
-	return email1.toLowerCase().trim() === email2.toLowerCase().trim();
+export const areEmailsEqual = (email1: string, secondEmail: string) => {
+	return email1.toLowerCase().trim() === secondEmail.toLowerCase().trim();
 };
